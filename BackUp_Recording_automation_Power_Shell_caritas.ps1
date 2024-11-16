dc9a5becdd538c53a38c6700a08d1bbf59a02419
Kyaovi01 <155931274+Kyaovi01@users.noreply.github.com>
2024-11-16 18:15:32 -0500

    Add files via upload

---
+++ BackUp_Recording_automation_Power_Shell_caritas.ps1
@@ -0,0 +1,19 @@
+﻿#Define source and destination paths 
+$source = "C:\Users"
+$currentUserDesktop = [Environement]::GetFolderPath("Desktop")
+$destination = "$currentUserDesktop\backup_$(Get-date -Format yyyyMMddhhmmss)"
+#Create a new directory  for today's backup 
+New-Item -ItemType Directory -Force -Path $destination
+
+#Copy all user Data to the backup directory 
+Get-ChildItem -Path $source -Recurse | Copy-Item -Destination {
+$destination +$_.FullName.Substring($source.Length)
+} -Container
+
+#Log the backup operation 
+$timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+$logFile = "$currentUserDesktop\backup_logs.txt"
+$logEntry = "$timestamp | Backup completed for source : $source to destination : $destination"
+Add-Content -Path $logFile -Value $logEntry
+
+Write-Output "Backup completed successfully" 
